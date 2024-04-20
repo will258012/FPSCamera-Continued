7931058a15991e9c3389aa0c4a53e855b3dba2ee
will258012 <will258012@gmail.com>
2024-04-20 18:03:27 +0800

    提升版本

--- FPSCamera/Properties/AssemblyInfo.cs
+++ FPSCamera/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FPSCamera")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("First Person Camera - Continued")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FPSCamera")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.*")]
+[assembly: AssemblyVersion("2.3.1.*")]
