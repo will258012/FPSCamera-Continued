668c1cb1cd084a61a4c42cc9e853de79a0e30c59
Will <125041335+will258012@users.noreply.github.com>
2024-12-01 15:10:16 +0800

    更新 FPSCameraAPI

--- FPSCamera.API/Detector.cs
+++ FPSCamera.API/Detector.cs
@@ -7,12 +7,20 @@ namespace FPSCameraAPI
 {
     public class Detector
     {
-        public static void CheckFPSCamera()
+        public static bool CheckFPSCamera()
         {
-            if (AccessTools.TypeByName("FPSCamera.Utils.ModSupport, FPSCamera") == null)
+            if (AssemblyUtils.GetEnabledAssembly("FPSCamera") == null ||
+                AccessTools.TypeByName("FPSCamera.Utils.ModSupport, FPSCamera") == null)
             {
-                Logging.Error("FPScamera not detected");
-
+                Logging.Error("FPSCamera not detected");
+                return false;
+            }
+            return true;
+        }
+        public static void ShowNotificationWhenFPSCameraIsNotDetected()
+        {
+            if (!CheckFPSCamera())
+            {
                 var notification = NotificationBase.ShowNotification<ListNotification>();
                 notification.AddParas(Translations.Translate("FPSCAMERA_NOT_DETECTED"));
             }
