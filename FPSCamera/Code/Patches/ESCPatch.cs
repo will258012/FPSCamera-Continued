cd21b56228052e69024533f486d2ea0a8a1bb4b9
Will <125041335+will258012@users.noreply.github.com>
2025-01-04 14:13:39 +0800

    修复ESC在编辑器中的触发问题

--- FPSCamera/Code/Patches/ESCPatch.cs
+++ FPSCamera/Code/Patches/ESCPatch.cs
@@ -3,11 +3,29 @@ using FPSCamera.UI;
 using HarmonyLib;
 namespace FPSCamera.Patches
 {
-    [HarmonyPatch(typeof(GameKeyShortcuts), "Escape")]
+    [HarmonyPatch]
     internal class EscHandler
     {
+
+        [HarmonyPatch(typeof(GameKeyShortcuts), "Escape")]
         [HarmonyPrefix]
-        public static bool ESCPatch() =>
+        static bool Prefix1() => Patch();
+        [HarmonyPatch(typeof(MapEditorKeyShortcuts), "Escape")]
+        [HarmonyPrefix]
+        static bool Prefix2() => Patch();
+        [HarmonyPatch(typeof(DecorationKeyShortcuts), "Escape")]
+        [HarmonyPrefix]
+        static bool Prefix3() => Patch();
+        [HarmonyPatch(typeof(GameKeyShortcuts), "SteamEscape")]
+        [HarmonyPrefix]
+        static bool Prefix4() => Patch();
+        [HarmonyPatch(typeof(MapEditorKeyShortcuts), "SteamEscape")]
+        [HarmonyPrefix]
+        static bool Prefix5() => Patch();
+        [HarmonyPatch(typeof(DecorationKeyShortcuts), "SteamEscape")]
+        [HarmonyPrefix]
+        static bool Prefix6() => Patch();
+        static bool Patch() =>
             // cancel calling <Escape> if FPSCamera consumes it
             !FPSCamController.Instance.OnEsc() && !MainPanel.Instance.OnEsc();
     }
