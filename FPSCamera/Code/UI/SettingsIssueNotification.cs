59c03d3da10d8480be910ef170b77bf17f813ae2
Will <125041335+will258012@users.noreply.github.com>
2025-04-12 22:32:37 +0800

    优化设置问题通知
    - 优化触发判定
    - “快速修复”仅重置通用偏移
    - 添加专门用于记录此通知“不再提示”的XML项

--- FPSCamera/Code/UI/SettingsIssueNotification.cs
+++ FPSCamera/Code/UI/SettingsIssueNotification.cs
@@ -1,10 +1,11 @@
-﻿namespace FPSCamera.UI
-{
-    using AlgernonCommons.Notifications;
-    using AlgernonCommons.Translation;
-    using ColossalFramework.UI;
-    using FPSCamera.Settings.Tabs;
+﻿using AlgernonCommons.Notifications;
+using AlgernonCommons.Translation;
+using AlgernonCommons.UI;
+using ColossalFramework.UI;
+using FPSCamera.Settings;
 
+namespace FPSCamera.UI
+{
     public class SettingsIssueNotification : ListNotification
     {
 
@@ -29,7 +30,16 @@
         public override void AddButtons()
         {
             base.AddButtons();
-            QuickFixButton = AddButton(2, NumButtons, Translations.Translate("SETTINGS_ISSUE_DETECTED_QUICKFIX"), () => { GeneralOptions.ResetModSettings(); Close(); });
+            QuickFixButton = AddButton(2, NumButtons, Translations.Translate("SETTINGS_ISSUE_DETECTED_QUICKFIX"),
+                () =>
+                {
+                    ModSettings.FollowCamOffset.y = 0;
+                    ModSettings.VehicleFixedOffset.y = 2;
+                    ModSettings.MidVehFixedOffset.y = 3;
+                    ModSettings.PedestrianFixedOffset.y = 2;
+                    OptionsPanelManager<OptionsPanel>.LocaleChanged();
+                    Close();
+                });
             // Add don't show again button.
             DSAButton = AddButton(3, NumButtons, Translations.Translate("NOTE_DONTSHOWAGAIN"), Close);
         }
