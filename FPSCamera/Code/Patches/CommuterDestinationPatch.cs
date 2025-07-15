6ab62bd7ef9cac417e4d9be1d60a3cf120d10025
Will <125041335+will258012@users.noreply.github.com>
2025-07-15 08:05:06 +0800

    详见描述
    - 修复当在启用 Commuter Destination 的面板时使用第一人称摄像机，按 ESC 禁用摄像机可能会导致面板关闭的问题

---
+++ FPSCamera/Code/Patches/CommuterDestinationPatch.cs
@@ -0,0 +1,37 @@
+﻿using AlgernonCommons;
+using ColossalFramework.UI;
+using FPSCamera.Cam.Controller;
+using FPSCamera.Game;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace FPSCamera.Patches
+{
+    [HarmonyPatch]
+
+    internal class CommuterDestinationPatch
+    {
+        private static bool shouldHide = true;
+        private static bool Prepare()
+        {
+            bool result = AssemblyUtils.IsAssemblyPresent("CommuterDestination.CS1");
+            if (result)
+                FPSCamController.OnCameraEnabled += () => shouldHide = false;
+            return result;
+        }
+        private static MethodBase TargetMethod() =>
+            AccessTools.Method(AccessTools.TypeByName("CommuterDestination.CS1.UI.StopDestinationInfoPanel, CommuterDestination.CS1"), "CheckForClose");
+        private static bool Prefix(UIPanel __instance)
+        {
+            if (KeyCode.Escape.KeyTriggered())
+            {
+                if (!shouldHide)
+                    shouldHide = true;
+                else
+                    __instance.Hide();
+            }
+            return false;
+        }
+    }
+}
